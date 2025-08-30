cd378d18e6fa866dcf130bfee6c3aa4ae8b1923f
Dan <generalgoldiez@gmail.com>
2025-08-30 07:51:53 +0100

    Initial Commit

---
+++ Z3st/Z3stApp.swift
@@ -0,0 +1,17 @@
+//
+//  Z3stApp.swift
+//  Z3st
+//
+//  Created by Daniel Block on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Z3stApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Z3stTests/Z3stTests.swift
@@ -0,0 +1,17 @@
+//
+//  Z3stTests.swift
+//  Z3stTests
+//
+//  Created by Daniel Block on 30/08/2025.
+//
+
+import Testing
+@testable import Z3st
+
+struct Z3stTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
